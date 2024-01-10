f84774ed23a5a9f028b71dcb589d110327e0956e
Jack Gardner <shortfilmsbyjack@aol.com>
2024-01-10 10:46:41 -0800

    Added method to extract text from image
    Added method to extract text from a PIL image using easyocr.

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,9 @@
 import time
 from PIL import Image
 import pyautogui
+import easyocr
 
+# Takes a screenshot of the entire screen
 def take_screenshot():
     # Get the current timestamp to use as part of the screenshot filename
     timestamp = time.strftime("%Y%m%d%H%M%S")
@@ -12,3 +14,18 @@ def take_screenshot():
     image.save(filename, format='PNG')
     # Take a screenshot and save it with the specified filename
     return filename
+
+# Extracts text from a image object using easyOCR
+def extract_text_from_image(image: Image, languages: list[str]):
+
+    reader = easyocr.Reader(languages)
+    
+    try:
+        # Use pytesseract to do OCR on the image
+        text = reader.readtext(image)
+
+        return text
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
